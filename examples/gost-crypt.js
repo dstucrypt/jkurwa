08cd1b820c8c363f1794dbf246862774dd03e4f5
Ilya Petrov <ilya.muromec@gmail.com>
2014-06-07 23:11:24 +0300

    Usage examples. Data encryption and decription.

---
+++ examples/gost-crypt.js
@@ -0,0 +1,47 @@
+var jk = require('../lib/index.js'),
+    em_gost = require('em-gost');
+
+/*
+   Data encryption with DSTU 4145 and GOST block cipher.
+   See gost-decrypt.js to decrypt this message.
+
+   keys:
+
+   reciever:
+     priv: 171b130e180d060f1f1a0807011816170f060b040a10121817111a0d0b0b0f03
+     pub:  e54bf3f92a281d02f46ad5637387a8f13c9698816cb4f8beadfc0565fa63d6b1
+
+   sender:
+     priv: 40a0e1400001e091b160101150f1b1e0f1d14130e1c0b07011d120a04120c041d
+     pub:  40ad1cb010531b504177577d8d4ad148219eb0c9bda61c9a0d4df1650d8cbe22
+
+*/
+
+// text we need to sign
+text = new Buffer("{'msg': 'hello', 'code': 1}");
+
+curve = jk.Curve.defined.DSTU_B_257;
+
+// agreed keys
+sender_priv = jk.Priv(curve, new jk.Big('40a0e1400001e091b160101150f1b1e0f1d14130e1c0b07011d120a04120c041d', 16));
+reciever_pub = jk.Pub(curve, curve.point(new jk.Big('e54bf3f92a281d02f46ad5637387a8f13c9698816cb4f8beadfc0565fa63d6b1', 16)));
+
+// random values choosen by sender
+//  ukm (salt, 64 bytes), iv (cipher param, 8 bytes), cek - 32 bytes key 
+
+ukm = new Buffer('0a151914091304101b1a140f1c1d0b1d091c121f07091e0d1a0118011b02171a0f0b001c14011401121a0e1b090305021b190b081c02121a1f1d0a04080b1418', 'hex');
+iv = new Buffer('09100509181c0515', 'hex');
+cek = new Buffer('11080811020a0d0913040f020111190b04060c101d1c0a0911060e160b121419', 'hex');
+
+// compute sharedkey and wrap encryption key
+sharedkey = new Buffer(sender_priv.sharedKey(reciever_pub, ukm, em_gost.gost_kdf));
+wcek = new Buffer(em_gost.gost_keywrap(sharedkey, cek, iv));
+
+// finally encrypt text
+ciphered = new Buffer(em_gost.gost_encrypt_cfb(text, cek, iv));
+
+console.log("sender should transmit following infomration to reciever: ");
+console.log("    wrapped key    : " + wcek.toString("hex"));
+console.log("    ukm            : " + ukm.toString("hex"));
+console.log("    iv             : " + iv.toString("hex"));
+console.log("    encrypted text : " + ciphered.toString("hex"));
---
+++ examples/gost-decrypt.js
@@ -0,0 +1,24 @@
+var jk = require('../lib/index.js'),
+    em_gost = require('em-gost');
+
+curve = jk.Curve.defined.DSTU_B_257; // standard curve
+
+// keys used
+reciever_priv = jk.Priv(curve, new jk.Big('171b130e180d060f1f1a0807011816170f060b040a10121817111a0d0b0b0f03', 16));
+sender_pub = jk.Pub(curve, curve.point(new jk.Big('40ad1cb010531b504177577d8d4ad148219eb0c9bda61c9a0d4df1650d8cbe22', 16)));
+
+// recived message
+// ukm (salt), iv (cipher param) and encrypted text
+ukm = new Buffer('0a151914091304101b1a140f1c1d0b1d091c121f07091e0d1a0118011b02171a0f0b001c14011401121a0e1b090305021b190b081c02121a1f1d0a04080b1418', 'hex');
+iv = new Buffer('09100509181c0515', 'hex');
+wcek = new Buffer('359a37cf972520b590ef109b7c454c991d95da782e30ac9fe917fbb52e9402a4d236fd030f49627ec63c2684', 'hex');
+ciphered = new Buffer('9fa6c038bf04e8810f7d3d25beb6fd9ee7efe0698e4f1216f17316', 'hex');
+
+// generate shared secret and unwrap message key
+sharedkey = reciever_priv.sharedKey(sender_pub, ukm, em_gost.gost_kdf);
+cek = new Buffer(em_gost.gost_unwrap(sharedkey, wcek, iv));
+
+// finally decrypt message
+text = new Buffer(em_gost.gost_decrypt_cfb(ciphered, cek, iv));
+
+console.log("deciphered text " + text);
