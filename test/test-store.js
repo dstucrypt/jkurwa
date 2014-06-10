651d1a20fe46d35b069d7e79eb29cee0ed33fac7
Ilya Petrov <ilya.muromec@gmail.com>
2014-06-10 23:42:57 +0300

    Priv model API changes
    - Curve.pkey() constructor now supports Big integers input
    - base64 decoder returns Buffer instance
    - Point.expand() uses pre-filled cache for common points
    - keycoder.parse() now returns list of Priv instances as
      well as Priv.from_asn1.

--- test/test-store.js
+++ test/test-store.js
@@ -40,6 +40,32 @@ var PEM_KEY2 = '-----BEGIN PRIVATE KEY-----\n' +
 describe('Keycoder', function () {
     var keycoder = new jk.Keycoder();
     var base_257 = new jk.Big('2a29ef207d0e9b6c55cd260b306c7e007ac491ca1b10c62334a9e8dcd8d20fb6', 16);
+
+    var check_257 = function (key) {
+        assert.equal(key.type, 'Priv');
+        assert.equal(key.curve.m, 257);
+        assert.equal(key.curve.ks, '12');
+        assert.equal(key.curve.a.toString(16), '0');
+        assert.equal(key.curve.b.toString(16), '1cef494720115657e18f938d7a7942394ff9425c1458c57861f9eea6adbe3be10');
+        assert.equal(key.curve.order.toString(16), '800000000000000000000000000000006759213af182e987d3e17714907d470d');
+        assert.equal(key.curve.base.toString(16), '<Point x:2a29ef207d0e9b6c55cd260b306c7e007ac491ca1b10c62334a9e8dcd8d20fb7, y:10686d41ff744d4449fccf6d8eea03102e6812c93a9d60b978b702cf156d814ef >');
+        assert.equal(key.d.toString(16), '1111111111111111111111111111111111111111111111111111111111111111');
+
+    };
+
+    var check_431 = function (key) {
+
+        assert.equal(key.type, 'Priv');
+        assert.equal(key.curve.m, 431);
+        assert.equal(key.curve.ks.toString(), [5, 3, 1]);
+        assert.equal(key.curve.a.toString(16), '1');
+        assert.equal(key.curve.b.toString(16), '3ce10490f6a708fc26dfe8c3d27c4f94e690134d5bff988d8d28aaeaede975936c66bac536b18ae2dc312ca493117daa469c640caf3');
+        assert.equal(key.curve.order.toString(16), '3fffffffffffffffffffffffffffffffffffffffffffffffffffffba3175458009a8c0a724f02f81aa8a1fcbaf80d90c7a95110504cf');
+        assert.equal(key.curve.base.toString(16), '<Point x:1a62ba79d98133a16bbae7ed9a8e03c32e0824d57aef72f88986874e5aae49c27bed49a2a95058068426c2171e99fd3b43c5947c857d, y:70b5e1e14031c1f70bbefe96bdde66f451754b4ca5f48da241f331aa396b8d1839a855c1769b1ea14ba53308b5e2723724e090e02db9 >');
+        assert.equal(key.d.toString(16), '888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888');
+
+    };
+
     describe('#parse()', function() {
         it("should parse encrypted key in PEM format", function () {
             var der = keycoder.maybe_pem(PEM_KEY),
@@ -49,29 +75,16 @@ describe('Keycoder', function () {
 
         it("should parse raw key in PEM format", function () {
             var der = keycoder.maybe_pem(PEM_KEY2),
-                keys = keycoder.parse(der),
-                key1 = keys.keys[0],
-                key2 = keys.keys[1];
+                store = keycoder.parse(der);
 
-            assert.equal(keys.format, 'privkeys')
-            assert.equal(key1.format, 'privkey')
-            assert.equal(key1.curve.m, 257);
-            assert.equal(key1.curve.k1, 12);
-            assert.equal(key1.curve.a.toString(16), '0');
-            assert.equal(key1.curve.b.toString(16), '1cef494720115657e18f938d7a7942394ff9425c1458c57861f9eea6adbe3be10');
-            assert.equal(key1.curve.order.toString(16), '800000000000000000000000000000006759213af182e987d3e17714907d470d');
-            assert.equal(key1.curve.base.toString(16), '2a29ef207d0e9b6c55cd260b306c7e007ac491ca1b10c62334a9e8dcd8d20fb6');
-            assert.equal(key1.param_d.toString(16), '1111111111111111111111111111111111111111111111111111111111111111');
+            assert.equal(store.format, 'privkeys')
+            check_257(store.keys[0]);
+            check_431(store.keys[1]);
 
-            assert.equal(key2.format, 'privkey')
-            assert.equal(key2.curve.m, 431);
-            assert.equal(key2.curve.ks.toString(), [5, 3, 1]);
-            assert.equal(key2.curve.a.toString(16), '1');
-            assert.equal(key2.curve.b.toString(16), '3ce10490f6a708fc26dfe8c3d27c4f94e690134d5bff988d8d28aaeaede975936c66bac536b18ae2dc312ca493117daa469c640caf3');
-            assert.equal(key2.curve.order.toString(16), '3fffffffffffffffffffffffffffffffffffffffffffffffffffffba3175458009a8c0a724f02f81aa8a1fcbaf80d90c7a95110504cf');
-            assert.equal(key2.curve.base.toString(16), '1a62ba79d98133a16bbae7ed9a8e03c32e0824d57aef72f88986874e5aae49c27bed49a2a95058068426c2171e99fd3b43c5947c857c');
-            assert.equal(key2.param_d.toString(16), '888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888');
-
+            store = jk.Priv.from_asn1(der);
+            assert.equal(store.format, 'privkeys')
+            check_257(store.keys[0]);
+            check_431(store.keys[1]);
         })
     });
 });
