d4481eb0e65ab9efe35eb4f74746283428d1ac8b
Ilya Petrov <ilya.muromec@gmail.com>
2014-08-30 02:07:56 +0300

    Porting to gf2m-lib

--- test/test-api.js
+++ test/test-api.js
@@ -1,7 +1,9 @@
 'use strict';
 
 var assert = require("assert"),
-    jk = require('../lib/index.js');
+    jk = require('../lib/index.js'),
+    Field = jk.Field;
+
 
 describe('API', function () {
     var curve = jk.std_curve('DSTU_PB_257'),
@@ -32,12 +34,12 @@ describe('API', function () {
             var pub = new jk.Pub(curve, curve.point(new jk.Big('e54bf3f92a281d02f46ad5637387a8f13c9698816cb4f8beadfc0565fa63d6b1', 16)));
 
             assert.equal(pub.type, 'Pub');
-            assert.equal(pub.point.x.value.equals(expect_pubx), true);
+            assert.equal(pub.point.x.equals(new Field(expect_pubx, 'bn', curve)), true);
 
 
             pub = curve.pubkey('e54bf3f92a281d02f46ad5637387a8f13c9698816cb4f8beadfc0565fa63d6b1');
             assert.equal(pub.type, 'Pub');
-            assert.equal(pub.point.x.value.equals(expect_pubx), true);
+            assert.equal(pub.point.x.equals(new Field(expect_pubx, 'bn', curve)), true);
 
 
         });
