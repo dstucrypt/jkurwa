32ad7a2035867d9ad5d5e07b61bb0015d98cd4c2
Illya Petrov <ilya.muromec@gmail.com>
2018-11-15 00:31:11 +0200

    move fixtures to separate file

--- test/test-store.js
+++ test/test-store.js
@@ -1,41 +1,8 @@
 'use strict';
 
 var jk = require('../lib/index.js'),
+    keys = require('./data/keys'),
     assert = require("assert");
-
-var PEM_KEY = '-----BEGIN ENCRYPTED PRIVATE KEY-----\n' +
-        'MIIBqjCBsAYJKoZIhvcNAQUNMIGiMEMGCSqGSIb3DQEFDDA2BCAxpY3BRimBGJz2' +
-        'xwHidsdVOlq19uNthBjkqkDJMM84dgICJxAwDgYKKoYkAgEBAQEBAgUAMFsGCyqG' +
-        'JAIBAQEBAQEDMEwECEuxD1wpRdSeBECp1utF8TxwgoDElnsjH16t9ljrpMA3KR04' +
-        '2WvwJcpOF/jpcg3GFbQ6KJdfC8Heo2Q4tWTqLBef0BI+bbj6xXkEBIH0KaIuKVHm' +
-        'MuHkRK449SHIkP9jd/wFORE6ZnIL/E6RB8VmoH4+q5rmfzN+2cZsAhNj55UIqf36' +
-        'CeeId9++dlQxYNyDGVQnqcf/L29A2ND+omWDxy725eIEXalRKmH7wrlXPosL3I8D' +
-        'TYzaOspjt4yYd/p1wih1a+dgg6I1JHoJTB7ymW/7/LRebRSAezjiaoYmEDUTHexj' +
-        's3MHtE7ywOr+UTks2KK4tQ/G+LyLGmLv0nbU6BuzWPSTG6qjZgwMC131LlIz2Q0f' +
-        'TvUgPEDwNs9ZEpFGYL8oISybP9kUHLibk8E1It6zMIWiXMECtbfbo3cHimReiA==\n' +
-        '-----END ENCRYPTED PRIVATE KEY-----';
-
-var PEM_KEY2 = '-----BEGIN PRIVATE KEY-----\n' +
-'MIIDiAIBADCByQYLKoYkAgEBAQEDAQEwgbkwdTAHAgIBAQIBDAIBAAQhEL7j22rq' +
-'nh+GV4xFwSWU/5QjlKfXOPkYfmUVAXKU9M4BAiEAgAAAAAAAAAAAAAAAAAAAAGdZ' +
-'ITrxgumH0+F3FJB9Rw0EIbYP0tjc6Kk0I8YQG8qRxHoAfmwwCybNVWybDn0g7ykq' +
-'AARAqdbrRfE8cIKAxJZ7Ix9erfZY66TANykdONlr8CXKThf46XINxhW0OiiXXwvB' +
-'3qNkOLVk6iwXn9ASPm24+sV5BAQgERERERERERERERERERERERERERERERERERER' +
-'ERERERGgggKTMDIGDCsGAQQBgZdGAQECATEiBCARERERERERERERERERERERERER' +
-'ERERERERERERERERETAyBgwrBgEEAYGXRgEBAgUxIgQgERERERERERERERERERER' +
-'EREREREREREREREREREREcAwSQYMKwYBBAGBl0YBAQIDMTkDNwMRERERERERERER' +
-'EREREREREREREREREREREREREREREREREREREREREREREREREREREREREREwggFZ' +
-'BgwrBgEEAYGXRgEBAgIxggFHMIIBQzCBvDAPAgIBrzAJAgEFAgEDAgEBAgEBBDYD' +
-'zhBJD2pwj8Jt/ow9J8T5TmkBNNW/+YjY0oqurt6XWTbGa6xTaxiuLcMSykkxF9qk' +
-'acZAyvMCNj///////////////////////////////////7oxdUWACajApyTwL4Gq' +
-'ih/Lr4DZDHqVEQUEzwQ2GmK6edmBM6Fruuftmo4Dwy4IJNV673L4iYaHTlquScJ7' +
-'7UmiqVBYBoQmwhcemf07Q8WUfIV8BECp1utF8TxwgoDElnsjH16t9ljrpMA3KR04' +
-'2WvwJcpOF/jpcg3GFbQ6KJdfC8Heo2Q4tWTqLBef0BI+bbj6xXkEBECp1utF8Txw' +
-'goDElnsjH16t9ljrpMA3KR042WvwJcpOF/jpcg3GFbQ6KJdfC8Heo2Q4tWTqLBef' +
-'0BI+bbj6xXkEMIGABgwrBgEEAYGXRgEBAgYxcDBuBEARERERERERERERERERERER' +
-'ERERERERERERERERERERERERERERERERERERERERERERERERERERERERERERERER' +
-'BAgREREREREREQQgERERERERERERERERERERERERERERERERERERERERERE=\n' +
-'-----END PRIVATE KEY-----' ;
 
 describe('Keycoder', function () {
     var b257 = jk.std_curve('DSTU_PB_257'),
@@ -59,23 +26,23 @@ describe('Keycoder', function () {
 
     describe('#parse()', function() {
         it("should parse encrypted key in PEM format", function () {
-            var key_store = jk.guess_parse(PEM_KEY);
+            var key_store = jk.guess_parse(keys.PEM_KEY_ENC);
             assert.equal(key_store.format, 'PBES2');
         });
 
         it("should parse raw key in PEM format", function () {
-            var store = jk.guess_parse(PEM_KEY2),
+            var store = jk.guess_parse(keys.PEM_KEY_RAW),
                 key;
 
             assert.equal(store.format, 'privkeys');
             check_257(store.keys[0]);
             check_431(store.keys[1]);
 
-            key = jk.Priv.from_pem(PEM_KEY2);
+            key = jk.Priv.from_pem(keys.PEM_KEY_RAW);
             assert.equal(key.type, 'Priv');
             check_257(key);
 
-            store = jk.Priv.from_pem(PEM_KEY2, true);
+            store = jk.Priv.from_pem(keys.PEM_KEY_RAW, true);
             assert.equal(store.format, 'privkeys');
             check_257(store.keys[0]);
             check_431(store.keys[1]);
