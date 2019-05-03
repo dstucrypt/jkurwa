8d45f71da532210389efbf87db06c7555bb43ad7
Illya Petrov <ilya.muromec@gmail.com>
2019-05-03 18:14:55 +0000

    Coverage for curve.js

--- test/test-api.js
+++ test/test-api.js
@@ -1,5 +1,6 @@
 /* eslint-env mocha */
 const assert = require("assert");
+const fs = require("fs");
 
 const jk = require("../lib/index.js");
 
@@ -14,6 +15,12 @@ describe("API", () => {
     curve
   );
 
+  const expectD431 = new jk.Field(
+    "e54bf3f92a281d02f46ad5637387a8f13c9698816cb440a0e1400001e091b160101150f1b1e0f1d14130e1c0b07011d120a04120c041d",
+    "hex",
+    curve
+  );
+
   const expectPubx = new jk.Field(
     "e54bf3f92a281d02f46ad5637387a8f13c9698816cb4f8beadfc0565fa63d6b0",
     "hex",
@@ -21,8 +28,8 @@ describe("API", () => {
   );
 
   describe("pkey()", () => {
-    it("should create private key from string", () => {
-      let priv = new jk.Priv(
+    it("should create private key from hex string through jk.Priv()", () => {
+      const priv = new jk.Priv(
         curve,
         new Field(
           "40a0e1400001e091b160101150f1b1e0f1d14130e1c0b07011d120a04120c041d",
@@ -32,14 +39,55 @@ describe("API", () => {
       );
       assert.equal(priv.type, "Priv");
       assert.equal(priv.d.equals(expectD), true);
+    });
 
-      priv = curve.pkey(
+    it("should create private key from hex string through curve.pkey()", () => {
+      const priv = curve.pkey(
         "40a0e1400001e091b160101150f1b1e0f1d14130e1c0b07011d120a04120c041d"
       );
       assert.equal(priv.type, "Priv");
       assert.equal(priv.d.equals(expectD), true);
+    });
 
-      priv = jk.pkey(
+    it("should read trinominal private key from asn1", () => {
+      const priv = jk.Priv.from_asn1(
+        fs.readFileSync(`${__dirname}/data/Key40A0.cer`)
+      );
+      assert.equal(priv.type, "Priv");
+      assert.equal(priv.d.equals(expectD), true);
+    });
+
+    it("should serialize trinominal private key to asn1", () => {
+      const priv = curve.pkey(
+        "40a0e1400001e091b160101150f1b1e0f1d14130e1c0b07011d120a04120c041d"
+      );
+      assert.deepEqual(
+        fs.readFileSync(`${__dirname}/data/Key40A0.cer`),
+        priv.to_asn1()
+      );
+    });
+
+    it("should serialize pentanominal private key to asn1", () => {
+      const curve431 = jk.std_curve("DSTU_PB_431");
+      const priv = curve431.pkey(
+        "e54bf3f92a281d02f46ad5637387a8f13c9698816cb440a0e1400001e091b160101150f1b1e0f1d14130e1c0b07011d120a04120c041d"
+      );
+      assert.deepEqual(
+        fs.readFileSync(`${__dirname}/data/KeyE54B.cer`),
+        priv.to_asn1()
+      );
+    });
+
+    it("should read pentanominal private key from asn1", () => {
+      const priv = jk.Priv.from_asn1(
+        fs.readFileSync(`${__dirname}/data/KeyE54B.cer`)
+      );
+      assert.equal(priv.type, "Priv");
+      assert.equal(priv.d.equals(expectD431), true);
+    });
+
+    it("should create private key from hex string through jk.pkey()", () => {
+      const priv = jk.pkey(
         "DSTU_PB_257",
         "40a0e1400001e091b160101150f1b1e0f1d14130e1c0b07011d120a04120c041d"
       );
@@ -47,11 +95,73 @@ describe("API", () => {
       assert.equal(priv.d.equals(expectD), true);
       assert.equal(curve, priv.curve);
     });
+
+    it("should create private key from u8 BE buffer through jk.pkey()", () => {
+      const buffer = [
+        4,
+        10,
+        14,
+        20,
+        0,
+        0,
+        30,
+        9,
+        27,
+        22,
+        1,
+        1,
+        21,
+        15,
+        27,
+        30,
+        15,
+        29,
+        20,
+        19,
+        14,
+        28,
+        11,
+        7,
+        1,
+        29,
+        18,
+        10,
+        4,
+        18,
+        12,
+        4,
+        29
+      ];
+
+      const priv = jk.pkey("DSTU_PB_257", buffer, "buf8");
+      assert.equal(priv.type, "Priv");
+      assert.equal(priv.d.equals(expectD), true);
+      assert.equal(curve, priv.curve);
+    });
+
+    it("should create private key from u32 LE buffer through jk.pkey()", () => {
+      const buffer = [
+        0x120c041d,
+        0x1d120a04,
+        0x1c0b0701,
+        0x1d14130e,
+        0x0f1b1e0f,
+        0x16010115,
+        0x001e091b,
+        0x0a0e1400,
+        0x4
+      ];
+
+      const priv = jk.pkey("DSTU_PB_257", buffer, "buf32");
+      assert.equal(priv.type, "Priv");
+      assert.equal(priv.d.equals(expectD), true);
+      assert.equal(curve, priv.curve);
+    });
   });
 
   describe("pubkey()", () => {
-    it("should create private key from hex string", () => {
-      let pub = new jk.Pub(
+    it("should create public key from hex string through jk.Pub()", () => {
+      const pub = new jk.Pub(
         curve,
         curve.point(
           "e54bf3f92a281d02f46ad5637387a8f13c9698816cb4f8beadfc0565fa63d6b1"
@@ -60,9 +170,82 @@ describe("API", () => {
 
       assert.equal(pub.type, "Pub");
       assert.equal(pub.point.x.equals(expectPubx), true);
+    });
 
-      pub = curve.pubkey(
+    it("should create public key from hex string through curve.pubkey", () => {
+      const pub = curve.pubkey(
         "e54bf3f92a281d02f46ad5637387a8f13c9698816cb4f8beadfc0565fa63d6b1"
+      );
+      assert.equal(pub.type, "Pub");
+      assert.equal(pub.point.x.equals(expectPubx), true);
+    });
+
+    it("should create public key from hex string through jk.pubkey()", () => {
+      const pub = jk.pubkey(
+        "DSTU_PB_257",
+        "e54bf3f92a281d02f46ad5637387a8f13c9698816cb4f8beadfc0565fa63d6b1"
+      );
+      assert.equal(pub.type, "Pub");
+      assert.equal(pub.point.x.equals(expectPubx), true);
+    });
+
+    it("should create public key from u8 BE buffer through jk.pubkey()", () => {
+      const pub = jk.pubkey(
+        "DSTU_PB_257",
+        [
+          229,
+          75,
+          243,
+          249,
+          42,
+          40,
+          29,
+          2,
+          244,
+          106,
+          213,
+          99,
+          115,
+          135,
+          168,
+          241,
+          60,
+          150,
+          152,
+          129,
+          108,
+          180,
+          248,
+          190,
+          173,
+          252,
+          5,
+          101,
+          250,
+          99,
+          214,
+          177
+        ],
+        "buf8"
+      );
+      assert.equal(pub.type, "Pub");
+      assert.equal(pub.point.x.equals(expectPubx), true);
+    });
+
+    it("should create public key from u32 LE buffer through jk.pubkey()", () => {
+      const pub = jk.pubkey(
+        "DSTU_PB_257",
+        [
+          0xfa63d6b1,
+          0xadfc0565,
+          0x6cb4f8be,
+          0x3c969881,
+          0x7387a8f1,
+          0xf46ad563,
+          0x2a281d02,
+          0xe54bf3f9
+        ],
+        "buf32"
       );
       assert.equal(pub.type, "Pub");
       assert.equal(pub.point.x.equals(expectPubx), true);
@@ -73,6 +256,22 @@ describe("API", () => {
     it("should return curve id as defined in z1399-12", () => {
       assert.equal(curve.curve_id(), 6);
       assert.equal(jk.std_curve("DSTU_PB_431").curve_id(), 9);
+    });
+  });
+
+  describe("equals()", () => {
+    it("should return true if curves are same", () => {
+      assert.equal(
+        jk.std_curve("DSTU_PB_257").equals(jk.std_curve("DSTU_PB_257")),
+        true
+      );
+    });
+
+    it("should return false if curves are different", () => {
+      assert.equal(
+        jk.std_curve("DSTU_PB_257").equals(jk.std_curve("DSTU_PB_431")),
+        false
+      );
     });
   });
 
